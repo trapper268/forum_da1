fb57d5ae3797a4e3ec9793c84e983547c0b3410c
huytt268 <tuanhuytruong35@gmail.com>
2024-12-03 20:55:29 +0700

    feat: Tailwind CSS setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-black text-white ">
+    <h1 className="text-3xl font-black text-light-500 ">
       Welcome to peso peso yes sir peso peso
     </h1>
   );
