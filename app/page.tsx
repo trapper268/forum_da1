8a205a02169578db3857cf262af1edd1aff469b3
huytt268 <tuanhuytruong35@gmail.com>
2024-12-03 22:30:37 +0700

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-black text-light-500 ">
-      Welcome to peso peso yes sir peso peso
-    </h1>
+    <>
+      <h1 className="h1-bold">Welcome to peso peso yes sir peso peso</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js
+      </h1>
+    </>
   );
 }
