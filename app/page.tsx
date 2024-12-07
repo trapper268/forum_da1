e7fc64f5f2c953a53ac8d7d0128a2d146db13de3
huytt268 <tuanhuytruong35@gmail.com>
2024-12-07 10:00:00 +0700

    Set up Light & Dark Theme, Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function Home() {
   return (
     <>
       <h1 className="h1-bold">Welcome to peso peso yes sir peso peso</h1>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to the world of Next.js
-      </h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 }
---
+++ context/Theme.tsx
@@ -0,0 +1,12 @@
+"use client";
+import {
+  ThemeProviderProps,
+  ThemeProvider as NextThemeProvider,
+} from "next-themes";
+import React from "react";
+
+const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+};
+
+export default ThemeProvider;
