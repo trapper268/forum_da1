d66330651e373bfb8f3c152dc000928e21ca0207
huytt268 <tuanhuytruong35@gmail.com>
2024-12-19 20:05:08 +0700

    Implement Create & Read User routes

---
+++ app/api/users/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validation";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const users = await User.find();
+
+    return NextResponse.json({ success: true, data: users }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+
+    const validateData = UserSchema.safeParse(body);
+
+    if (!validateData.success) {
+      throw new ValidationError(validateData.error.flatten().fieldErrors);
+    }
+
+    const { email, username } = validateData.data;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) throw new Error("User already exists");
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) throw new Error("Username already exists");
+
+    const newUser = await User.create(validateData.data);
+
+    return NextResponse.json({ success: true, data: newUser }, { status: 201 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
